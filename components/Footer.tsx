163ce9866a8bcf1a4510471f20bcea600732b12a
SamsonCanon <samsoncanon2018@gmail.com>
2025-01-13 15:03:39 +0100

    bug fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className=" bg-primary  text-white py-5 px-4">
-      <footer className=" max-width md:max-w-[1000px]  gap-5 md:flex md:justify-between md:items-center ">
+      <footer className=" max-width md:max-w-[1000px]  gap-5 flex-col md:flex-row flex md:justify-between md:items-center ">
         <Image
           src="/icons/Logo.svg"
           width={100}
@@ -11,7 +11,7 @@ const Footer = () => {
           alt="entapp tech logo"
           className=" w-auto  h-[32px] "
         />
-        <div className=" text-[12px]   text-center ">
+        <div className=" text-[12px]  text-center ">
           <span>
             <span className=" text-secondary">ENTAPP</span> Copyright © 2025 All
             Right Reserved
